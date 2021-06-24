4261aabc489fa13349bd0770447124f26427e419
Sunil <sunilthedj@gmail.com>
2021-06-24 23:03:36 +0530

    Form Started for Module Generation

--- src/app/generator/crud-module/crud-module.component.ts
+++ src/app/generator/crud-module/crud-module.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormControl, FormGroup, Validators} from "@angular/forms";
+import {NzFormTooltipIcon} from "ng-zorro-antd/form";
 
 @Component({
   selector: 'app-crud-module',
@@ -7,9 +9,43 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CrudModuleComponent implements OnInit {
 
-  constructor() { }
+  validateForm!: FormGroup;
+  captchaTooltipIcon: NzFormTooltipIcon = {
+    type: 'info-circle',
+    theme: 'twotone'
+  };
+
+  submitForm(): void {
+    for (const i in this.validateForm.controls) {
+      this.validateForm.controls[i].markAsDirty();
+      this.validateForm.controls[i].updateValueAndValidity();
+    }
+  }
+
+  updateConfirmValidator(): void {
+    /** wait for refresh value */
+    Promise.resolve().then(() => this.validateForm.controls.checkPassword.updateValueAndValidity());
+  }
+
+  confirmationValidator = (control: FormControl): { [s: string]: boolean } => {
+    if (!control.value) {
+      return { required: true };
+    } else if (control.value !== this.validateForm.controls.password.value) {
+      return { confirm: true, error: true };
+    }
+    return {};
+  };
+
+  getCaptcha(e: MouseEvent): void {
+    e.preventDefault();
+  }
+
+  constructor(private fb: FormBuilder) {}
 
   ngOnInit(): void {
+    this.validateForm = this.fb.group({
+      moduleName: ['', [Validators.required]]
+    });
   }
 
 }
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -4,12 +4,28 @@ import {NzLayoutModule} from "ng-zorro-antd/layout";
 import {NzBreadCrumbModule} from "ng-zorro-antd/breadcrumb";
 import {NzMenuModule} from "ng-zorro-antd/menu";
 import {NzDropDownModule} from "ng-zorro-antd/dropdown";
-
+import { NzFormModule } from 'ng-zorro-antd/form';
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import {NzCheckboxModule} from "ng-zorro-antd/checkbox";
+import {NzInputModule} from "ng-zorro-antd/input";
+import {NzSelectModule} from "ng-zorro-antd/select";
+import {NzButtonModule} from "ng-zorro-antd/button";
+import {NzCardModule} from "ng-zorro-antd/card";
+import {NzDividerModule} from "ng-zorro-antd/divider";
 const imports:any[] = [
   NzLayoutModule,
   NzBreadCrumbModule,
   NzMenuModule,
-  NzDropDownModule
+  NzDropDownModule,
+  NzFormModule,
+  FormsModule,
+  ReactiveFormsModule,
+  NzCheckboxModule,
+  NzInputModule,
+  NzSelectModule,
+  NzButtonModule,
+  NzCardModule,
+  NzDividerModule
 ];
 @NgModule({
   declarations: [],
